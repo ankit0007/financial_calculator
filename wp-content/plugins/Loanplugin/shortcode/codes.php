27bebb68cf21b190ded4c033611af9f359d18292
Ankit Saxena <ankit.friend07@gmail.com>
2023-12-26 23:07:39 +0530

    Revert "canges by Amat mam"
    This reverts commit f6feb783dff0a6270e008b129c80d8a376d50603.

--- wp-content/plugins/Loanplugin/shortcode/codes.php
+++ wp-content/plugins/Loanplugin/shortcode/codes.php
@@ -9,83 +9,31 @@ function callback_financial_calculator($atts, $content = null) {
     $CLASS = substr($CLASS, 1, 15);
     $HEADING_TOTAL = 'Total Amount Repaid';
     $HEADING_MONTHLY = 'Monthly Repayment';
-    $subheading = "hello";
-    $paragraph="";
     $HTML = "<div class=' mainbox " . $CLASS . " " . $CALCOLOR . " financial_type_" . $CALTYPE . "' data-function='" . $CALTYPE . "'>";
 
     $HTML .= "<div class='" . $CLASS . " title'>" . CalculatorTitle($CALTYPE) . " Calculator</div>";
     $HTML .= "<div class='row boxesinMobile'>";
     $HTML .= "<div class='whitebox col-8'>";
-    
+    $HTML .= "<div class='subheading'>Calculate how much a personal loan would cost and what the monthly repayments could be. </div>";
+    $HTML .= "<div class='calculatorInner row d-flex justify-content'>";
     switch ($CALTYPE) {
         case 'mortgage_payments':
-            $paragraph="*Our mortgage loan calculator is for illustrative purposes to give you an approximate idea how much a loan could cost you. It is not intended to give any indication or guarantee of acceptance. Any application you may then choose to make will be subject to credit and other checks.";
-            $HTML .= "<div class='subheading'>Calculate how much a mortgage loan would cost and what the monthly repayments could be.</div>";
-            $HTML .= "<div class='calculatorInner row d-flex justify-content'>";
             $HTML .= "<div class='col PropertyValue'><label>Property Value (" . Currency($CURRENCY) . ")</label><input class='validation' data-max='4000000' type='text'></div>"
-                    . "<div class='col LoanTerm'><label>Loan Term (months)</label>
-                        <select name='month' id='month' class='validation'>
-                            <option value='1'>1</option>
-                            <option value='2'>2</option>
-                            <option value='3'>3</option>
-                            <option value='4'>4</option>
-                            <option value='5'>5</option>
-                            <option value='6'>6</option>
-                            <option value='7'>7</option>
-                            <option value='8'>8</option>
-                            <option value='9'>9</option>
-                            <option value='10'>10</option>
-                            <option value='11'>11</option>
-                            <option value='12' selected>12</option>
-                        </select>
-                    </div>"
-                    . "<div class='col APR'><label>APR (%)</label>
-                        <select name='apr' id='apr' class='validation'>
-                            <option value='29.5'>29.5</option>
-                        </select>
-                    </div>"
+                    . "<div class='col LoanTerm'><label>Loan Term (months)</label><input class='validation' data-max='360' type='text'></div>"
+                    . "<div class='col APR'><label>APR (%)</label><input class='validation' data-max='20' type='text'></div>"
                     . "<div class='col deposit'><label>deposit (" . Currency($CURRENCY) . ")</label><input class='diposit' data-max='4000000' type='text'></div>"
                     . "<div class='col btns'><label>deposit</label><input type='button' value='Calculate'></div>";
             break;
         case 'business_loan':
-            $paragraph="*Our personal business loan calculator is for illustrative purposes to give you an approximate idea how much a loan could cost you. It is not intended to give any indication or guarantee of acceptance. Any application you may then choose to make will be subject to credit and other checks.";
-            $HTML .= "<div class='subheading'>Calculate how much a business loan would cost and what the monthly repayments could be.</div>";
-            $HTML .= "<div class='calculatorInner row d-flex justify-content'>";
             $HTML .= "<div class='col Amount'><label>Amount (" . Currency($CURRENCY) . ")</label><input class='validation' data-max='5000000' type='text'></div>"
-                    . "<div class='col LoanTerm'><label>Loan Term (months)</label>
-                    <select name='month' id='month' class='validation'>
-                        <option value='1'>1</option>
-                        <option value='2'>2</option>
-                        <option value='3'>3</option>
-                        <option value='4'>4</option>
-                        <option value='5'>5</option>
-                        <option value='6'>6</option>
-                        <option value='7'>7</option>
-                        <option value='8'>8</option>
-                        <option value='9'>9</option>
-                        <option value='10'>10</option>
-                        <option value='11'>11</option>
-                        <option value='12' selected>12</option>
-                    </select>
-                </div>"
-                . "<div class='col APR'><label>APR (%)</label>
-                    <select name='apr' id='apr' class='validation'>
-                        <option value='29.5'>29.5</option>
-                    </select>
-                </div>"
+                    . "<div class='col LoanTerm'><label>Loan Term (months)</label><input class='validation' data-max='96' type='text'></div>"
+                    . "<div class='col APR'><label>APR (%)</label><input  class='validation' data-max='30'  type='text'></div>"
                     . "<div class='col btns'><label>deposit</label><input type='button' value='Calculate'></div>"
                     . "<div class='col btns'><label>deposit</label></div>";
             break;
         case 'balance_transfer':
-            $paragraph="* This calculation is an estimated savings based on the assumptions that you have provided. Your actual savings may vary depending on the balance transfer offer you receive and the payment schedule you maintain while in your introductory period.";
-            $HTML .= "<div class='subheading'>Calculate how much you can save with balance transfer?</div>";
-            $HTML .= "<div class='calculatorInner row d-flex justify-content'>";
             $HTML .= "<div class='col Amount'><label>Amount (" . Currency($CURRENCY) . ")</label><input type='text'></div>"
-                    . "<div class='col APR'><label>APR (%)</label>
-                    <select name='apr' id='apr' class='validation'>
-                        <option value='29.5'>29.5</option>
-                    </select>
-                </div>"
+                    . "<div class='col APR'><label>APR (%)</label><input class='validation' data-max='20' type='text'></div>"
                     . "<div class='col aproffset'><label>apr offset</label><input type='text'></div>"
                     . "<div class='col paymentamount'><label>payment amount (" . Currency($CURRENCY) . ")</label><input type='text'></div>"
                     . "<div class='col'></div></div>"
@@ -93,58 +41,22 @@ function callback_financial_calculator($atts, $content = null) {
                     . "<div class='col btns'><label>deposit</label><input type='button' value='Calculate'></div><div class='col'></div><div class='col'></div><div class='col'></div>";
             break;
         case 'life_of_balance_card_repayments':
-            $paragraph="*Calculated on approximate minimum payment rates which vary by provider and product. We assume no additional spend on the card, no fees are incurred and the same static interest rate is paid on all balances.";
-            $HTML .= "<div class='subheading'>Calculate how much a card will cost you or how quickly you can pay off your existing cards. Simply add a card below to get started.</div>";
-            $HTML .= "<div class='calculatorInner row d-flex justify-content'>";
             $HTML .= "<div class='col amount'><label>amount (" . Currency($CURRENCY) . ")</label><input type='text'></div>"
-                    . "<div class='col APR'><label>APR (%)</label>
-                    <select name='apr' id='apr' class='validation'>
-                        <option value='29.5'>29.5</option>
-                    </select>
-                </div>"
+                    . "<div class='col APR'><label>APR (%)</label><input class='validation' data-max='20' type='text'></div>"
                     . "<div class='col paymenttype'><label>payment type</label><input type='text'></div>"
                     . "<div class='col paymentamount'><label>payment amount (" . Currency($CURRENCY) . ")</label><input type='text'></div>"
                     . "<div class='col btns'><label>deposit</label><input type='button' value='Calculate'></div>";
             break;
         case 'car_financing':
-            $paragraph="*This calculator is for illustration purposes only. Your actual repayments could be lower or higher depending on your personal circumstances. The make, model and age of the car you’d like to purchase will also impact the final figure quoted.";
-            $HTML .= "<div class='subheading'>Calculate how much a car loan would cost and what the monthly repayments could be.</div>";
-            $HTML .= "<div class='calculatorInner row d-flex justify-content'>";
             $HTML .= "<div class='col amount'><label>amount (" . Currency($CURRENCY) . ")</label><input class='validation' data-max='10000000'  type='text'></div>"
-                    . "<div class='col LoanTerm'><label>Loan Term (months)</label>
-                    <select name='month' id='month' class='validation'>
-                        <option value='1'>1</option>
-                        <option value='2'>2</option>
-                        <option value='3'>3</option>
-                        <option value='4'>4</option>
-                        <option value='5'>5</option>
-                        <option value='6'>6</option>
-                        <option value='7'>7</option>
-                        <option value='8'>8</option>
-                        <option value='9'>9</option>
-                        <option value='10'>10</option>
-                        <option value='11'>11</option>
-                        <option value='12' selected>12</option>
-                    </select>
-                </div>"
-                . "<div class='col APR'><label>APR (%)</label>
-                    <select name='apr' id='apr' class='validation'>
-                        <option value='29.5'>29.5</option>
-                    </select>
-                </div>"
+                    . "<div class='col LoanTerm'><label>Loan Term (months)</label><input class='validation' data-max='360' type='text'></div>"
+                    . "<div class='col APR'><label>APR (%)</label><input class='validation' data-max='30' type='text'></div>"
                     . "<div class='col deposit'><label>deposit (" . Currency($CURRENCY) . ")</label><input class='validation' data-max='10000000'  type='text'></div>"
                     . "<div class='col btns'><label>deposit</label><input type='button' value='Calculate'></div>";
             break;
         case 'credit_card_repayments':
-            $paragraph="*Calculated on approximate minimum payment rates which vary by provider and product. We assume no additional spend on the card, no fees are incurred and the same static interest rate is paid on all balances.";
-            $HTML .= "<div class='subheading'>Calculate how much a credit card will cost you or how quickly you can pay off your existing cards. Simply add a card below to get started.</div>";
-            $HTML .= "<div class='calculatorInner row d-flex justify-content'>";
             $HTML .= "<div class='col amount'><label>amount (" . Currency($CURRENCY) . ")</label><input class='validation' data-max='100000' type='text'></div>"
-                    . "<div class='col APR'><label>APR (%)</label>
-                    <select name='apr' id='apr' class='validation'>
-                        <option value='29.5'>29.5</option>
-                    </select>
-                </div>"
+                    . "<div class='col APR'><label>APR (%)</label><input class='validation' data-max='39' type='text'></div>"
                     . "<div class='col paymenttype'><label>payment type</label><input type='text'></div>"
                     . "<div class='col paymentamount'><label>payment amount (" . Currency($CURRENCY) . ")</label><input class='validation' data-max='50000' type='text'></div>"
                     . "<div class='col btns'><label>deposit</label><input type='button' value='Calculate'></div>";
@@ -152,59 +64,15 @@ function callback_financial_calculator($atts, $content = null) {
             $HEADING_MONTHLY = 'Loan Term (Months)';
             break;
         case 'hcstc_loan':
-            $paragraph="* This calculation is an estimated savings based on the assumptions that you have provided. Your actual savings may vary depending on the balance transfer offer you receive and the payment schedule you maintain while in your introductory period.";
-            $HTML .= "<div class='subheading'>Calculate how much a HCSTC loan would cost and what the monthly repayments could be.</div>";
-            $HTML .= "<div class='calculatorInner row d-flex justify-content'>";
             $HTML .= "<div class='col amount'><label>amount (" . Currency($CURRENCY) . ")</label><input class='validation' data-max='4000000' type='text'></div>"
-                    . "<div class='col LoanTerm'><label>Loan Term (months)</label>
-                    <select name='month' id='month' class='validation'>
-                        <option value='1'>1</option>
-                        <option value='2'>2</option>
-                        <option value='3'>3</option>
-                        <option value='4'>4</option>
-                        <option value='5'>5</option>
-                        <option value='6'>6</option>
-                        <option value='7'>7</option>
-                        <option value='8'>8</option>
-                        <option value='9'>9</option>
-                        <option value='10'>10</option>
-                        <option value='11'>11</option>
-                        <option value='12' selected>12</option>
-                    </select>
-                </div>"
-                . "<div class='col APR'><label>APR (%)</label>
-                    <select name='apr' id='apr' class='validation'>
-                        <option value='29.5'>29.5</option>
-                    </select>
-                </div>"
+                    . "<div class='col LoanTerm'><label>Loan Term (months)</label><input class='validation' data-max='60' type='text'></div>"
+                    . "<div class='col APR'><label>APR (%)</label><input class='validation' data-max='21' type='text'></div>"
                     . "<div class='col btns'><label>deposit</label><input class='validation' min='0' data-max='4000000' type='button' value='Calculate'></div>";
             break;
         case 'secured_loan':
-            $paragraph="* This calculation is an estimated savings based on the assumptions that you have provided. Your actual savings may vary depending on the balance transfer offer you receive and the payment schedule you maintain while in your introductory period.";
-            $HTML .= "<div class='subheading'>Calculate how much a secured loan would cost and what the monthly repayments could be.</div>";
-            $HTML .= "<div class='calculatorInner row d-flex justify-content'>";
             $HTML .= "<div class='col amount'><label>amount (" . Currency($CURRENCY) . ")</label><input class='validation' data-max='105000000' type='text'></div>"
-                    . "<div class='col LoanTerm'><label>Loan Term (months)</label>
-                    <select name='month' id='month' class='validation'>
-                        <option value='1'>1</option>
-                        <option value='2'>2</option>
-                        <option value='3'>3</option>
-                        <option value='4'>4</option>
-                        <option value='5'>5</option>
-                        <option value='6'>6</option>
-                        <option value='7'>7</option>
-                        <option value='8'>8</option>
-                        <option value='9'>9</option>
-                        <option value='10'>10</option>
-                        <option value='11'>11</option>
-                        <option value='12' selected>12</option>
-                    </select>
-                </div>"
-                . "<div class='col APR'><label>APR (%)</label>
-                    <select name='apr' id='apr' class='validation'>
-                        <option value='29.5'>29.5</option>
-                    </select>
-                </div>"
+                    . "<div class='col LoanTerm'><label>Loan Term (months)</label><input class='validation' data-max='180' type='text'></div>"
+                    . "<div class='col APR'><label>APR (%)</label><input class='validation' data-max='14' type='text'></div>"
                     . "<div class='col propertyvalue'><label>property value (" . Currency($CURRENCY) . ")</label><input class='validation' data-max='100000000' type='text'></div>"
                     . "<div class='col'></div></div>"
                     . "<div class='row calculatorInner row d-flex justify-content'><div class='col mortgagebalance'><label>mortgage balance (" . Currency($CURRENCY) . ")</label><input type='text'></div>"
@@ -218,7 +86,7 @@ function callback_financial_calculator($atts, $content = null) {
     $HTML .= "<div class='col20per col-4'>";
     $HTML .= '<div class="row total"><div class="col-8">' . $HEADING_TOTAL . '</div><div class="col-4 totalpay">' . Currency($CURRENCY) . '<span>0.0</span></div></div>';
     $HTML .= '<div class="row subtotal"><div class="col-8">' . $HEADING_MONTHLY . '</div><div class="col-4 permonthpay">' . Currency($CURRENCY) . '<span>0.0</span></div></div>';
-    $HTML .= "<div class='note'>".$paragraph."</div>";
+    $HTML .= "<div class='note'>*Our personal loan calculator is for illustrative purposes to give you an approximate idea how much a loan could cost you. It is not intended to give any indication or guarantee of acceptance. Any application you may then choose to make will be subject to credit and other checks.</div>";
     $HTML .= "</div></div></div>";
 
     return $HTML;
