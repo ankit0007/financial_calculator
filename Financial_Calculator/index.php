06155003e9ef6d943d49e8921ba72b190e23ed0d
Ankit Saxena <ankit.friend07@gmail.com>
2024-03-26 17:39:45 +0530

    up

--- Financial_Calculator/index.php
+++ Financial_Calculator/index.php
@@ -3,7 +3,7 @@
  * 
  * Plugin Name: Financial Calculators
  * Version:     1.0
- * Plugin URI:  https://seomasterteam.com
+ * Plugin URI:  https://github.com/ankit0007/financial_calculator/blob/main/Financial_Calculator/readme.txt
  * Description: Introducing the Financial Calculator plugin, your all-in-one solution for precise financial planning. This versatile tool empowers users with essential calculators, including mortgage payments, business loans, balance transfers, car financing, credit card repayments, and secured loans. Seamlessly integrated into your platform, this plugin simplifies complex financial computations, ensuring users make informed decisions. Whether planning a home purchase, business expansion, or managing debts, the Financial Calculator provides accurate insights. With its user-friendly interface and comprehensive features, it's the go-to tool for individuals and businesses alike. Elevate your platform's financial functionality with the Financial Calculator plugin, making financial management a breeze.
  * Author:      Team seomasterteam
  * Author URI:  https://seomasterteam.com
